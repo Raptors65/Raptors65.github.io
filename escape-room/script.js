6e9c49ffc26e0b3535b1ba03a92cc90353dff777
Lucas Kim <lucaskamadakim@gmail.com>
2022-06-23 21:50:14 -0400

    Add question 8

--- escape-room/script.js
+++ escape-room/script.js
@@ -13,9 +13,9 @@ MathJax = {
           const left = parser.GetBrackets(name, '');
           const size = parser.GetBrackets(name, '16px');
           const w = parser.GetBrackets(name, '5em');
-          const value = parser.GetArgument(name);
+          const placeholder = parser.GetArgument(name);
           xml.setXML(MathJax.startup.adaptor.node('input', {
-            id: id, type: 'number', value: value, style: {left, position: "relative", fontSize: size, width: w}, xmlns: 'http://www.w3.org/1999/xhtml'
+            id: id, type: 'number', placeholder, style: {left, position: "relative", fontSize: size, width: w}, xmlns: 'http://www.w3.org/1999/xhtml'
           }), MathJax.startup.adaptor);
           xml.getSerializedXML = function () {
             return this.adaptor.outerHTML(this.xml) + '</input>';
